27785751d583a39031aa2f1a420bb7fb60c600e2
zhangyycn <zhangyycn@hotmail.com>
2019-06-25 10:43:17 +0800

    third

--- src/Main.ts
+++ src/Main.ts
@@ -1,12 +1,14 @@
 import GameConfig from "./GameConfig";
+import UnityExportTest from "./script/UnityExportTest"
 import TransformDemo from "./script/TransformDemo";
 import Sprite3DLoad from "./script/Sprite3DLoad";
 import MaterialAnimation from "./script/MaterialAnimation"
 
 class Main {
     constructor() {
+        new UnityExportTest();
         //new TransformDemo();
-        new Sprite3DLoad();
+        //new Sprite3DLoad();
         //new MaterialAnimation();
     }
 }
