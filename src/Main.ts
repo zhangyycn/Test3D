7cd8206b1b2245475da72afa46242b7d489733ec
zhangyycn <zhangyycn@hotmail.com>
2019-06-25 10:38:19 +0800

    second

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,13 @@
 import GameConfig from "./GameConfig";
 import TransformDemo from "./script/TransformDemo";
 import Sprite3DLoad from "./script/Sprite3DLoad";
+import MaterialAnimation from "./script/MaterialAnimation"
 
 class Main {
     constructor() {
         //new TransformDemo();
         new Sprite3DLoad();
+        //new MaterialAnimation();
     }
 }
 //激活启动类
--- src/script/Sprite3DLoad.ts
+++ src/script/Sprite3DLoad.ts
@@ -1,3 +1,27 @@
+import CameraMoveScript from "./common/CameraMoveScript"
+
+export default class Sprite3DLoad {
+    constructor() {
+        Laya3D.init(0, 0);
+        Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
+        Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
+        Laya.Stat.show();
+        
+        var scene:Laya.Scene3D = Laya.stage.addChild(new Laya.Scene3D()) as Laya.Scene3D;
+        scene.ambientColor = new Laya.Vector3(1, 1, 1);
+        
+        var camera:Laya.Camera = scene.addChild(new Laya.Camera(0, 0.1, 100)) as Laya.Camera;
+        camera.transform.translate(new Laya.Vector3(0, 0.5, 1));
+        camera.transform.rotate(new Laya.Vector3( -15, 0, 0), true, false);
+        camera.addComponent(CameraMoveScript);
+        
+        Laya.Sprite3D.load("res/threeDimen/skinModel/LayaMonkey/LayaMonkey.lh", Laya.Handler.create(null, function(sprite:Laya.Sprite3D):void {
+            scene.addChild(sprite);
+        }));
+    }
+}
+
+/*
 export default class Sprite3DLoad {
     private scene: Laya.Scene3D;
     private camera: Laya.Camera;
@@ -76,3 +100,4 @@ export default class Sprite3DLoad {
         this.role.transform.rotate(this.rotate, false, false);
     }
 }
+*/
