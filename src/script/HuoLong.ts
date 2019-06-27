70ba8ca78f34e36680128a2b65474f390fac4097
zhangyycn <zhangyycn@hotmail.com>
2019-06-27 19:51:26 +0800

    commit;

--- src/script/HuoLong.ts
+++ src/script/HuoLong.ts
@@ -8,6 +8,7 @@ export default class HuoLong {
     private aniState: Laya.AnimatorState;
     private actionName: string[] = ["wait", "run", "skill1", "skill2", "hurt", "die"];
     private state: number = 0;
+    private move: number = 0.05;
 
 	constructor() {
 		Laya3D.init(0, 0);
@@ -30,10 +31,20 @@ export default class HuoLong {
 
             Laya.stage.on(Laya.Event.CLICK, this, this.onClick);
         }));
+
+        Laya.timer.frameLoop(20, this, this.onFrame);
     }
 
     onClick() {
         this.state = ++this.state > 5 ? 0 : this.state;
         this.animator.play(this.actionName[this.state]);
+    }
+
+    onFrame() {
+        if (this.role.transform.position.x >= 3) this.move = -0.05;
+        if (this.role.transform.position.x <= -3) this.move = 0.05;
+
+        this.role.transform.translate(new Laya.Vector3(this.move, 0, this.move));
+        console.log(this.role.transform.position)
     }
 }
